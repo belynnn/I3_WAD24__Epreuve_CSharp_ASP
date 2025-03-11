171ef7bf264812ffc0d9245e44c5f503e515f359
Deborah Clerckx <d.clerckx@interface3.be>
2025-03-11 11:24:28 +0100

    update : ajout contrainte check de l'état du jeu dans l'emprunt, si INCOMPLET, ne récupère pas

--- DB/Tables/Emprunt.sql
+++ DB/Tables/Emprunt.sql
@@ -5,12 +5,15 @@
     [EmprunteurId] INT NOT NULL,
     [DateEmprunt] DATETIME NOT NULL DEFAULT GETDATE(),
     [DateRetour] DATETIME NULL,
+	[EtatIdNom] NVARCHAR(50) NOT NULL,
     [EvaluationPreteur] INT ,
     [EvaluationEmprunteur] INT ,
     CONSTRAINT PK_Emprunt PRIMARY KEY ([EmpruntId]),
+	CONSTRAINT FK_Emprunt_Etat FOREIGN KEY ([EtatIdNom]) REFERENCES [dbo].[Etat]([Nom]) ON DELETE NO ACTION,
     CONSTRAINT FK_Emprunt_Jeu FOREIGN KEY ([JeuId]) REFERENCES [dbo].[Jeux]([JeuId]) ON DELETE CASCADE,
     CONSTRAINT FK_Emprunt_Preteur FOREIGN KEY ([PreteurId]) REFERENCES [dbo].[Utilisateur]([UtilisateurId]) ON DELETE NO ACTION,
     CONSTRAINT FK_Emprunt_Emprunteur FOREIGN KEY ([EmprunteurId]) REFERENCES [dbo].[Utilisateur]([UtilisateurId]) ON DELETE NO ACTION,
     CONSTRAINT CK_Emprunt_EvaluationPreteur CHECK ([EvaluationPreteur] BETWEEN 0 AND 5),
-    CONSTRAINT CK_Emprunt_EvaluationEmprunteur CHECK ([EvaluationEmprunteur] BETWEEN 0 AND 5)
+    CONSTRAINT CK_Emprunt_EvaluationEmprunteur CHECK ([EvaluationEmprunteur] BETWEEN 0 AND 5),
+	CONSTRAINT CK_Emprunt_Etat CHECK ([EtatIdNom] NOT IN ('Incomplet'))
 );
