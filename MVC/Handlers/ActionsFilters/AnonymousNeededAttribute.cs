b39d97b5305f6a112d25d29f0782664552775c1e
Deborah Clerckx <d.clerckx@interface3.be>
2025-03-12 14:21:06 +0100

    update : ajout des ActionsFilters liée à la connexion & anonymous

---
+++ MVC/Handlers/ActionsFilters/AnonymousNeededAttribute.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using MVC.Handlers;
+
+namespace MVC.Handlers.ActionFilters
+{
+	[AttributeUsage(AttributeTargets.Method)]
+	public class AnonymousNeededAttribute : Attribute, IActionFilter
+	{
+		public void OnActionExecuted(ActionExecutedContext context)
+		{
+			return;
+		}
+
+		public void OnActionExecuting(ActionExecutingContext context)
+		{
+			if (context.HttpContext.Session.GetString(nameof(SessionManager.ConnectedUser)) is not null)
+			{
+				context.Result = new RedirectToActionResult("Index", "Home", null);
+			}
+		}
+	}
+}
