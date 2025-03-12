1dc51751d1b9561b9e0d8bff745fa51582118b2c
Deborah Clerckx <d.clerckx@interface3.be>
2025-03-12 12:14:18 +0100

    update : ajout model User -> userdetail & userlistitem

---
+++ MVC/Models/User/UserDetails.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace MVC.Models.User
+{
+	public class UserDetails
+	{
+		[ScaffoldColumn(false)]
+		public int UtilisateurId { get; set; }
+
+		[DisplayName("Pseudo : ")]
+		public string Pseudo { get; set; }
+
+		[DisplayName("E-mail : ")]
+		[EmailAddress]
+		public string Email { get; set; }
+
+		[DisplayName("Date d'inscription : ")]
+		[DataType(DataType.Date)]
+		public DateOnly DateCreation { get; set; }
+	}
+}
---
+++ MVC/Models/User/UserListItem.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace MVC.Models.User
+{
+	public class UserListItem
+	{
+		[ScaffoldColumn(false)]
+		public int UtilisateurId { get; set; }
+
+		[DisplayName("Pseudo")]
+		public string Pseudo { get; set; }
+	}
+}
