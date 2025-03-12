e3b6cb9d91b49a1687de05f65c09a1203fb9b2e0
Deborah Clerckx <d.clerckx@interface3.be>
2025-03-12 10:44:45 +0100

    update : ajout du user mapper + link reference avec Common & DAL

--- BLL/Mappers/Mapper.cs
+++ BLL/Mappers/Mapper.cs
@@ -3,10 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BLL.Entities;
+using D = DAL.Entities;
 
 namespace BLL.Mappers
 {
-	internal class Mapper
+	internal static class Mapper
 	{
+		public static User ToBLL(this D.User user)
+		{
+			if (user is null) throw new ArgumentNullException(nameof(user));
+			return new User(
+				user.UtilisateurId,
+				user.Pseudo,
+				user.Email,
+				user.MotDePasse,
+				user.DateCreation,
+				user.DateDesactivation/*,
+				user.Role*/);
+		}
+
+		public static D.User ToDAL(this User user)
+		{
+			if (user is null) throw new ArgumentNullException(nameof(user));
+			return new D.User()
+			{
+				UtilisateurId = user.UtilisateurId,
+				Pseudo = user.Pseudo,
+				Email = user.Email,
+				MotDePasse = user.MotDePasse,
+				DateCreation = user.DateCreation,
+				DateDesactivation = (user.IsDisabled) ? new DateTime() : null/*,
+				Role = user.Role.ToString()*/
+			};
+		}
 	}
 }
