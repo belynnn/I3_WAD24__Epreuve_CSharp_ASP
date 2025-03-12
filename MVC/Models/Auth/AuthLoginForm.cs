b14387b5a712ca7956b8ad012492e49b869aa0d4
Deborah Clerckx <d.clerckx@interface3.be>
2025-03-12 16:18:11 +0100

    update : MVC : fin de journée - ajout du AuthController avec son Model et ses vue Login/Logout

---
+++ MVC/Models/Auth/AuthLoginForm.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace MVC.Models.Auth
+{
+	public class AuthLoginForm
+	{
+		[DisplayName("E-mail : ")]
+		[Required(ErrorMessage = "Le champ 'E-mail' est obligatoire.")]
+		[EmailAddress]
+		public string Email { get; set; }
+
+		[DisplayName("Mot de passe : ")]
+		[Required(ErrorMessage = "Le champ 'Mot de passe' est obligatoire.")]
+		[MinLength(8, ErrorMessage = "Le champ 'Mot de passe' doit contenir au minimum 16 caractères.")]
+		[MaxLength(32, ErrorMessage = "Le champ 'Mot de passe' doit contenir au maximum 32 caractères.")]
+		[RegularExpression(@"^.*(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\-_+=\.()\[\]$µ£\\/*§?{}]).*", ErrorMessage = "Le champ 'Mot de passe' doit contenir au minimum une minuscule, une majuscule, un chiffre et un symbole.")]
+		[DataType(DataType.Password)]
+		public string MotDePasse { get; set; }
+	}
+}
