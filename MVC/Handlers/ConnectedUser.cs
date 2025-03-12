bec681862d0cfa65079416e6ebe10945bdca6685
Deborah Clerckx <d.clerckx@interface3.be>
2025-03-12 12:14:43 +0100

    update : ajout dossier handlers pour gérer la session et les utilisateurs connectés

---
+++ MVC/Handlers/ConnectedUser.cs
@@ -0,0 +1,10 @@
+﻿namespace MVC.Handlers
+{
+	public class ConnectedUser
+	{
+		public int UtilisateurId { get; set; }
+		public string Email { get; set; }
+		public DateTime ConnectedAt { get; set; }
+		//public string Role { get; set; }
+	}
+}
