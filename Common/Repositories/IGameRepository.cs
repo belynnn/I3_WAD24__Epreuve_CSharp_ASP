ae36c746fabc0ee34571277e9e408d5c66bcfd0f
Deborah Clerckx <d.clerckx@interface3.be>
2025-03-13 12:13:22 +0100

    update : DAL GAME : ajout interface gamerepository

---
+++ Common/Repositories/IGameRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Repositories
+{
+	public interface IGameRepository<TGame> : ICRUDRepository<TGame, int>
+	{
+	}
+}
