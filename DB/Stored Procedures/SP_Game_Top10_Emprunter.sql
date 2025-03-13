fd0ed7f5f67a1157d82c4611c54a62b910697fb6
Deborah Clerckx <d.clerckx@interface3.be>
2025-03-13 16:19:54 +0100

    update DB : test ajout procédure Top10

---
+++ DB/Stored Procedures/SP_Game_Top10_Emprunter.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[SP_Game_Top10_Emprunter]
+AS
+BEGIN
+    SELECT TOP 10 
+        J.JeuId,
+        J.Nom,
+        J.Description,
+        COUNT(E.JeuId) AS BorrowCount
+    FROM [dbo].[Jeux] AS J
+    INNER JOIN [dbo].[Emprunt] AS E ON J.JeuId = E.JeuId
+    GROUP BY J.JeuId, J.Nom, J.Description
+    ORDER BY COUNT(E.JeuId) DESC;
+END
