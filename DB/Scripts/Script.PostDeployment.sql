f0181fdd9fffebeaaa8c73a185a2c81fe30cb3ea
Deborah Clerckx <d.clerckx@interface3.be>
2025-03-13 12:17:51 +0100

    update : DAL GAME : Test DAL fonctionnelle (+ suppression d'un commentaire)

--- DB/Scripts/Script.PostDeployment.sql
+++ DB/Scripts/Script.PostDeployment.sql
@@ -1,15 +1,4 @@
-﻿/*
-Post-Deployment Script Template							
---------------------------------------------------------------------------------------
- This file contains SQL statements that will be appended to the build script.		
- Use SQLCMD syntax to include a file in the post-deployment script.			
- Example:      :r .\myfile.sql								
- Use SQLCMD syntax to reference a variable in the post-deployment script.		
- Example:      :setvar TableName MyTable							
-               SELECT * FROM [$(TableName)]					
---------------------------------------------------------------------------------------
-*/
--- 1. Insertion dans la table Etat (table indépendante)
+﻿-- 1. Insertion dans la table Etat (table indépendante)
 INSERT INTO [dbo].[Etat] ([Nom])
 VALUES
     ('Neuf'),
